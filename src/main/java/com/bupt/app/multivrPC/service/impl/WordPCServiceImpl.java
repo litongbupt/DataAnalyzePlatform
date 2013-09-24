a3ad2b9863892686dc626f2a2dac59e09c51c988
litong <litong@sogou-inc.com>
2013-09-24 11:28:15 +0800

    修正一些BUG

--- src/main/java/com/bupt/app/multivrPC/service/impl/WordPCServiceImpl.java
+++ src/main/java/com/bupt/app/multivrPC/service/impl/WordPCServiceImpl.java
@@ -295,7 +295,7 @@ public class WordPCServiceImpl implements WordPCService {
 
 	@Override
 	public List<Integer> getPositionList() {
-		return Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
+		return Arrays.asList(new Integer[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
 	}
 
 	@Override
